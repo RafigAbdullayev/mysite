18a10c73e98133f24e47abc6ae57e50432937f40
RafigAbdullayev <168650900+RafigAbdullayev@users.noreply.github.com>
2024-05-16 02:45:15 +0400

    Add files via upload

---
+++ veriAlma.php
@@ -0,0 +1,9 @@
+<?php
+
+if(isset) {
+    echo "Adınız: " . $_POST["isim"] "<br>"
+    echo "Soyadınız: " . $_POST["soyisim"] "<br>"
+    echo "E-Mail: " . $_POST["email"] "<br>"
+    echo "Konu: " . $_POST["konu"] 
+
+}
